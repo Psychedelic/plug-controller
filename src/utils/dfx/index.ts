a3646937bb6a87c4660ea8d11311c5b7686e784d
Tomas Rocchi <tomasrocchi@Tomass-MacBook-Pro.local>
2021-06-01 13:27:57 -0300

    Added ledger interfaces

--- src/utils/dfx/index.ts
+++ src/utils/dfx/index.ts
@@ -1,7 +1,10 @@
-import { HttpAgent } from "@dfinity/agent";
+import { HttpAgent, Actor, ActorSubclass, Principal } from "@dfinity/agent";
 import { Ed25519KeyIdentity } from "@dfinity/identity";
 import fetch from "cross-fetch";
 import { config } from 'dotenv';
+
+import WalletService from '../../interfaces/wallet';
+import ledgerIDLFactory from '../../idls/ledger.did';
 
 if (process.env.NODE_ENV !== 'production') config();
 
@@ -15,5 +18,29 @@ export const createAgent = async (secretKey: Uint8Array) => {
         await agent.fetchRootKey();
         return agent;
         });
-    return agent;
+    return { agent, identity };
 }
+
+export const getLedgerActor = async (secretKey: Uint8Array) => {
+    const { agent, identity } = await createAgent(secretKey)
+  
+    const actor = Actor.createActor<ActorSubclass<WalletService>>(
+      ledgerIDLFactory,
+      { agent, canisterId: process.env.LEDGER_CANISTER_ID! }
+    );
+  
+    console.log("principal id", identity.getPrincipal().toText());
+  
+    actor.wallet_balance().then((res) => {
+      console.log("wallet balanace", res);
+  
+      actor
+        .wallet_call({
+          canister: Principal.fromText("bqdjp-7iaaa-aaaad-qabgq-cai"),
+          method_name: "canister_status",
+          cycles: 0 as unknown as bigint,
+          args: [],
+        })
+        .then(console.log);
+    });
+  };
