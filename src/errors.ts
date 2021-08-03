305233991086199d2404e181a1dcec677ccdd5df
aleacevedo <ale.acevedo@live.com.ar>
2021-08-03 19:15:42 +0200

    feat: add some errors

--- src/errors.ts
+++ src/errors.ts
@@ -1,5 +1,6 @@
 export const ERRORS = {
   INVALID_MNEMONIC: 'The provided mnemonic is invalid',
+  INVALID_PEM: 'The provided pem is invalid',
   INVALID_ACC_ID: 'The account ID should be a positive integer',
   PASSWORD_REQUIRED: 'A password is required',
   NOT_INITIALIZED: 'Plug must be initialized',
