4f45e3b10abb51517c4f438f5f8a432eb13ed6a4
rocky-fleek <tomas@fleek.co>
2021-11-03 10:04:03 -0300

    Modified host url and bumped minor version

--- src/utils/dfx/constants.ts
+++ src/utils/dfx/constants.ts
@@ -1,6 +1,6 @@
 export const LEDGER_CANISTER_ID = 'ryjl3-tyaaa-aaaaa-aaaba-cai';
 export const NNS_CANISTER_ID = 'qoctq-giaaa-aaaaa-aaaea-cai';
-export const IC_HOST = 'https://ic0.app';
+export const IC_HOST = 'https://plugproxy-test.fleek.co/';
 export const NET_ID = {
   blockchain: 'Internet Computer',
   network: '00000000000000020101',
