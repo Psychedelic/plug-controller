99fd8c6a32370d89814bffdc2820d686e136b9a5
rocky-fleek <tomas@fleek.co>
2021-11-03 16:10:30 -0300

    Added wrapped fetch function with header check

--- src/utils/dfx/index.ts
+++ src/utils/dfx/index.ts
@@ -7,7 +7,7 @@ import { BinaryBlob, blobFromUint8Array } from '@dfinity/candid';
 import Secp256k1KeyIdentity from '../crypto/secpk256k1/identity';
 
 // import TokenService from '../../interfaces/token';
-import { IC_HOST } from './constants';
+import { IC_URL_HOST, PLUG_PROXY_HOST } from './constants';
 
 export interface CreateAgentArgs {
   secretKey: BinaryBlob;
@@ -17,6 +17,45 @@ export interface CreateAgentArgs {
 export const createIdentity = (secretKey: BinaryBlob): Secp256k1KeyIdentity =>
   Secp256k1KeyIdentity.fromSecretKey(secretKey);
 
+let use_ic_url = false;
+
+/* eslint-disable no-param-reassign */
+const wrappedFetchInternal = (resolve, reject, resource, ...initArgs): void => {
+  if (use_ic_url) {
+    resource = new URL(resource);
+    resource.host = IC_URL_HOST;
+  }
+  console.log('fetching', resource);
+  fetch(resource, ...initArgs)
+    .then(r => {
+      if (!use_ic_url && r.headers['x-unavailable']) {
+        use_ic_url = true;
+        wrappedFetchInternal(resolve, reject, resource, ...initArgs);
+        return;
+      }
+      resolve(r);
+    })
+    .catch(e => {
+      reject(e);
+    });
+};
+
+export const wrappedFetch = (
+  ...args: Parameters<typeof fetch>
+): Promise<Response> => {
+  let reject;
+  let resolve;
+
+  const promise = new Promise((_resolve, _reject) => {
+    resolve = _resolve;
+    reject = _reject;
+  });
+
+  wrappedFetchInternal(resolve, reject, ...args);
+
+  return promise as Promise<Response>;
+};
+
 export const createAgent = async ({
   secretKey,
   defaultIdentity,
@@ -24,7 +63,11 @@ export const createAgent = async ({
   const identity =
     defaultIdentity || createIdentity(blobFromUint8Array(secretKey));
   const agent = await Promise.resolve(
-    new HttpAgent({ host: process.env.DFX_HOST || IC_HOST, fetch, identity })
+    new HttpAgent({
+      host: process.env.DFX_HOST || PLUG_PROXY_HOST,
+      fetch: wrappedFetch,
+      identity,
+    })
   ).then(async ag => {
     await ag.fetchRootKey();
     return ag;
