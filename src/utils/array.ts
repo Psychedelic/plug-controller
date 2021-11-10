3c780cde7c95f05739450795afa3aa5dd937af6e
aleacevedo <ale.acevedo@live.com.ar>
2021-11-10 17:28:55 -0300

    feat: add some extra utils

--- src/utils/array.ts
+++ src/utils/array.ts
@@ -3,10 +3,39 @@ export const uniqueByObjKey = (arr: Array<any>, key: string): Array<any> => [
   ...new Map(arr.map(item => [item[key], item])).values(),
 ];
 
-export const fromHexToUint8Array = (hexString: string): Uint8Array =>
-  new Uint8Array(
-    hexString.match(/.{1,2}/g)?.map(byte => parseInt(byte, 16)) || []
+/*
+ * Return an array buffer from its hexadecimal representation.
+ * @param hexString The hexadecimal string.
+ */
+export function fromHexToUint8Array(hexString: string): Uint8Array {
+  return new Uint8Array(
+    (hexString.match(/.{1,2}/g) ?? []).map(byte => parseInt(byte, 16))
   );
+}
 
-export const fromUint8ArrayToHex = (bytes: Uint8Array): string =>
-  bytes.reduce((str, byte) => str + byte.toString(16).padStart(2, '0'), '');
+/**
+ * Returns an hexadecimal representation of an array buffer.
+ * @param bytes The array buffer.
+ */
+export function fromUint8ArrayToHex(bytes: Uint8Array): string {
+  return bytes.reduce(
+    (str, byte) => str + byte.toString(16).padStart(2, '0'),
+    ''
+  );
+}
+
+/**
+ * Return an array buffer from its hexadecimal representation.
+ * @param hexString The hexadecimal string.
+ */
+export function fromHexToArrayBuffer(hexString: string): ArrayBuffer {
+  return fromHexToUint8Array(hexString).buffer;
+}
+
+/**
+ * Returns an hexadecimal representation of an array buffer.
+ * @param bytes The array buffer.
+ */
+export function fromArrayBufferToHex(bytes: ArrayBuffer): string {
+  return fromUint8ArrayToHex(new Uint8Array(bytes));
+}
