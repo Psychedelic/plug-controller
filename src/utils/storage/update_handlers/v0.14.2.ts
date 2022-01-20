2517932d33dd0d6162860b78db1b63789a12f1ac
aleacevedo <ale.acevedo@live.com.ar>
2022-01-20 18:18:05 -0300

    feat: manage storage update

---
+++ src/utils/storage/update_handlers/v0.14.2.ts
@@ -0,0 +1,23 @@
+import { TOKENS } from "../../../constants/tokens";
+import { StorageData } from "../../../interfaces/storage"
+
+export default (storage: any) : StorageData => ({
+    ...storage,
+    valut: {
+        ...storage.vault,
+        wallets: storage.value.wallets.map((wallet) => ({
+            ...wallet,
+            assets: wallet.assets.reduce((acum, asset) => ({...acum, [asset.canisterId]: {
+                balance: '0',
+                token: {
+                    name: asset.name,
+                    symbol: asset.symbol,
+                    canisterId: asset.canisterId,
+                    standard:  wallet.registeredTokens[asset.canisterId]?.standard || TOKENS[asset.symbol].standard,
+                    decimals: wallet.registeredTokens[asset.canisterId]?.decimals || TOKENS[asset.symbol].decimals,
+                    color: wallet.registeredTokens[asset.canisterId]?.color
+                }
+            }}), {})
+        }))
+    }
+});
