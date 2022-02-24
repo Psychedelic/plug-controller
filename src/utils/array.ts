3b8e7b1cb35d5d8f5c1833ee8506a99ca97153d3
rocky-fleek <tomas@fleek.co>
2022-02-24 13:01:31 -0300

    WIP: Missing decimal parsing

--- src/utils/array.ts
+++ src/utils/array.ts
@@ -2,3 +2,7 @@
 export const uniqueByObjKey = (arr: Array<any>, key: string): Array<any> => [
   ...new Map(arr.map(item => [item[key], item])).values(),
 ];
+
+export function uniqueMap<T, K>(array: Array<T>, mapFunction: (item: T) => K | undefined): Array<K> {
+  return [...new Set(array.map(mapFunction))].filter(value => value) as Array<K>;
+};
--- src/utils/dfx/history/cap.ts
+++ src/utils/dfx/history/cap.ts
@@ -3,13 +3,15 @@
 import axios, { AxiosResponse } from 'axios';
 import { Principal } from '@dfinity/principal';
 import { prettifyCapTransactions } from '@psychedelic/cap-js';
-import { getTokens, getAllNFTS } from '@psychedelic/dab-js';
+import { getTokens, getAllNFTS, TokenRegistry } from '@psychedelic/dab-js';
 
 import { getCanisterInfo } from '../../dab';
 import { recursiveParseBigint } from '../../object';
 import { lebDecode } from '../../crypto/binary';
 
 import { InferredTransaction } from '../../../interfaces/transactions';
+import { uniqueMap } from '../../array';
+import { XTC_ID } from '../constants';
 
 const KYASHU_URL = 'https://kyasshu.fleek.co';
 
@@ -52,42 +54,99 @@ const parsePrincipal = pidObj =>
 const getTransactionCanister = (contract: string): string | undefined =>
   contract?.split('#')?.[1];
 
-const parseBySymbol = (amount: bigint, symbol: string): string => {
-  switch (symbol) {
-    case 'WICP':
-      return amount.toString();
-    default:
-      return amount.toString();
-  }
-};
-
-const formatTransaction = (
+const formatTransaction = async (
   transaction: any,
-  sk: string,
-  contractId: string
-): InferredTransaction => {
-  const amount = transaction.details?.amount;
+  canistersInfo
+): Promise<InferredTransaction> => {
+  const canisterId = getTransactionCanister(transaction.contractId);
+  const prettifyEvent = prettifyCapTransactions(transaction.event);
+  if (canisterId) {
+    prettifyEvent.details = {
+      ...prettifyEvent.details,
+      canisterInfo: canistersInfo[canisterId],
+    };
+  }
+  const amount = prettifyEvent?.details?.amount;
   const parsedAmount =
     amount instanceof Array && !amount.some(value => typeof value !== 'number')
       ? lebDecode(Uint8Array.from(amount as Array<number>))
       : amount;
-  const symbol = transaction?.details?.['tokenRegistryInfo']?.symbol;
-  return {
-    hash: sk,
-    timestamp: transaction.time,
-    type: transaction.operation,
+  const getHandledTokenInfo = async (principal) => {
+    if (!principal) return;
+    const canisterId = parsePrincipal(principal);
+    if (canistersInfo[canisterId]?.tokenRegistryInfo) return canistersInfo[canisterId]?.tokenRegistryInfo;
+    else {
+      const registry = new TokenRegistry();
+      const data = await registry.get(canisterId);
+      return data;
+    }
+  }
+  const tokenId = prettifyEvent?.details?.tokenId || prettifyEvent?.details?.token || '';
+  const buildSonicData = async () => {
+    console.log(prettifyEvent?.details?.balance?.toString(), parsedAmount, amount)
+    const isSwap = prettifyEvent?.operation?.toLowerCase?.()?.includes?.('swap');
+    let data: any = { token: await getHandledTokenInfo(tokenId), amount: prettifyEvent?.details?.amount };
+    if (isSwap) {
+      const [from, to] = (prettifyEvent?.details?.pairId as string)?.split(':');
+      data.swap = {
+        from: await getHandledTokenInfo(from),
+        to: await getHandledTokenInfo(to),
+        amountIn: prettifyEvent?.details?.amountIn,
+        amountOut: prettifyEvent?.details?.amountOut
+      };
+    }
+    return data;
+  }
+  return recursiveParseBigint({
+    hash: transaction.sk,
+    timestamp: prettifyEvent?.time,
+    type: prettifyEvent?.operation,
     details: {
-      ...recursiveParseBigint(transaction.details),
-      amount:
-        parsedAmount && symbol ? parseBySymbol(parsedAmount, symbol) : amount,
-      canisterId: getTransactionCanister(contractId),
-      tokenId:
-        transaction.details?.token_id || transaction.details?.token || '',
-      to: parsePrincipal(transaction?.details?.to),
-      from: parsePrincipal(transaction?.details?.from),
+      ...prettifyEvent?.details,
+      amount: parsedAmount,
+      canisterId,
+      tokenId,
+      to: parsePrincipal(prettifyEvent?.details?.to),
+      from: parsePrincipal(prettifyEvent?.details?.from),
+      ...(canisterId === "3xwpq-ziaaa-aaaah-qcn4a-cai" ? {
+        sonicData: await buildSonicData()
+      } : {}),
     },
-    caller: parsePrincipal(transaction.caller) || '',
-  };
+    caller: parsePrincipal(prettifyEvent?.caller) || '',
+  });
+};
+
+const metadataArrayToObject = (metadata) => metadata.reduce((acum, item) =>
+  ({ ...acum, [item.principal_id.toString()]: item }), {});
+
+
+const getCanistersInfo = async (canisterIds: string[]): Promise<any> => {
+  const dabTokensInfo = metadataArrayToObject(await getTokens());
+  const dabNFTsInfo = metadataArrayToObject(await getAllNFTS({}));
+  const dabInfo = await Promise.all(
+    canisterIds.map(async canisterId => {
+      let canisterInfo = { canisterId };
+      // First check if present in nft or token registries
+      if (dabTokensInfo[canisterId])
+        canisterInfo['tokenRegistryInfo'] = dabTokensInfo[canisterId];
+      if (dabNFTsInfo[canisterId])
+        canisterInfo['nftRegistryInfo'] = dabNFTsInfo[canisterId];
+      try {
+        // Fetch extra metadata from canister registry
+        const fetchedCanisterInfo = await getCanisterInfo(canisterId);
+        canisterInfo = { ...canisterInfo, ...fetchedCanisterInfo };
+      } catch (error) {
+        /* eslint-disable-next-line */
+        console.error('DAB error: ', error);
+      }
+      return canisterInfo;
+    })
+  );
+  const canistersInfo = dabInfo.reduce(
+    (acum, info) => ({ ...acum, [info.canisterId]: info }),
+    {}
+  );
+  return canistersInfo;
 };
 
 export const getCapTransactions = async (
@@ -99,55 +158,9 @@ export const getCapTransactions = async (
   }`;
   try {
     const response = await axios.get<any, AxiosResponse<KyashuResponse>>(url);
-    const canisterIds = [
-      ...new Set(
-        response.data.Items.map(item => getTransactionCanister(item.contractId))
-      ),
-    ].filter(value => value) as string[];
-    const dabTokensInfo = (await getTokens()).reduce(
-      (acum, token) => ({ ...acum, [token.principal_id.toString()]: token }),
-      {}
-    );
-    const dabNFTsInfo = (await getAllNFTS({})).reduce(
-      (acum, token) => ({ ...acum, [token.principal_id.toString()]: token }),
-      {}
-    );
-    const dabInfo = await Promise.all(
-      canisterIds.map(async canisterId => {
-        let canisterInfo = { canisterId };
-        if (dabTokensInfo[canisterId])
-          canisterInfo['tokenRegistryInfo'] = dabTokensInfo[canisterId];
-        if (dabNFTsInfo[canisterId])
-          canisterInfo['nftRegistryInfo'] = dabNFTsInfo[canisterId];
-        try {
-          const fetchedCanisterInfo = await getCanisterInfo(canisterId);
-          canisterInfo = { ...canisterInfo, ...fetchedCanisterInfo };
-        } catch (error) {
-          /* eslint-disable-next-line */
-          console.error('DAB error: ', error);
-        }
-        return canisterInfo;
-      })
-    );
-    const canistersInfo = dabInfo.reduce(
-      (acum, info) => ({ ...acum, [info.canisterId]: info }),
-      {}
-    );
-    const transactions = response.data.Items.map(item => {
-      const canisterId = getTransactionCanister(item.contractId);
-      const prettifyEvent = prettifyCapTransactions(item.event);
-      if (canisterId)
-        prettifyEvent['details'] = {
-          ...prettifyEvent['details'],
-          ...canistersInfo[canisterId],
-        };
-      const formattedTx = formatTransaction(
-        prettifyEvent,
-        item.sk,
-        item.contractId
-      );
-      return formattedTx;
-    });
+    const canisterIds = uniqueMap<KyashuItem, string>(response.data.Items, item => getTransactionCanister(item.contractId));
+    const canistersInfo = await getCanistersInfo(canisterIds);
+    const transactions = await Promise.all(response.data.Items.map(async item => formatTransaction(item,canistersInfo)));
     return {
       total: response.data.Count,
       lastEvaluatedKey: response.data.LastEvaluatedKey,
