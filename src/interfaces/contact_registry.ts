0086a8e614d0eb5eb8464b2bec2c567abbfa360d
Juan Manuel <44899916+angarita-dev@users.noreply.github.com>
2022-05-20 11:26:28 -0500

    Added contact registry types

---
+++ src/interfaces/contact_registry.ts
@@ -0,0 +1,18 @@
+import { Principal } from '@dfinity/principal';
+
+export type ValueType = { 'PrincipalId': Principal } | { 'AccountId': string } | { 'Icns': string }
+
+export interface Address {
+  'name': string,
+  'description': [] | [string],
+  'emoji': [] | [string],
+  'value': ValueType,
+}
+
+export type Error = { 'NotAuthorized' : null } |
+{ 'BadParameters' : null } |
+{ 'Unknown' : string } |
+{ 'NonExistentItem' : null };
+
+export type Response = { 'Ok' : [] | [string] } |
+  { 'Err' : Error };
