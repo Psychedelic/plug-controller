a9ec7112d071581bd04315b22deef392a88baf52
Dante <dante@Dantes-MacBook-Pro.local>
2022-10-03 16:41:16 -0300

    Add eof and JsonnableKeyPair type in genericSignIdentity

--- src/utils/identity/genericSignIdentity.ts
+++ src/utils/identity/genericSignIdentity.ts
@@ -1,11 +1,12 @@
 import { SignIdentity } from '@dfinity/agent';
+import { JsonnableKeyPair } from './../../interfaces/identity'
 
 
 export abstract class GenericSignIdentity extends SignIdentity {
    /**
     * Serialize this key to JSON.
     */
-    abstract toJSON(): [string, string];
+    abstract toJSON(): JsonnableKeyPair;
 
    /**
     *  Return private key in a pem file
