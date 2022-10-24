275e496ffd87038671b29019f33589ffe48ac266
Dante <dante@Dantes-MacBook-Pro.local>
2022-10-24 09:55:18 -0300

    Add strict pem validation

--- src/utils/identity/parsePem.ts
+++ src/utils/identity/parsePem.ts
@@ -4,15 +4,27 @@ import Ed25519KeyIdentity from './ed25519/ed25519Identity';
 import { Types } from '../account/constants';
 import { ERRORS } from '../../errors';
 
+const ED25519_KEY_INIT = '3053020101300506032b657004220420';
+const SEC256k1_KEY_INIT = '30740201010420';
+
+const ED25519_KEY_SEPARATOR = 'a123032100';
+const SEC256k1_KEY_SEPARATOR = 'a00706052b8104000aa144034200';
+
 export const parseEd25519 = (pem: string) => {
 
-  const raw = Buffer.from(pem, "base64")
-    .toString("hex")
-    .replace("3053020101300506032b657004220420", "")
-    .replace("a123032100", "");
+  const raw = Buffer.from(pem, 'base64')
+    .toString('hex')
+
+    if (!raw.substring(0, 32).includes(ED25519_KEY_INIT)) {
+      return undefined;
+    }
+
+    const trimRaw = raw
+      .replace(ED25519_KEY_INIT, '')
+      .replace(ED25519_KEY_SEPARATOR, '');
 
   try {
-    const key = new Uint8Array(Buffer.from(raw, "hex"));
+    const key = new Uint8Array(Buffer.from(trimRaw, 'hex'));
     const identity = Ed25519KeyIdentity.fromSecretKey(key);
     const type = Types.pem25519;
     return { identity, type };
@@ -23,13 +35,19 @@ export const parseEd25519 = (pem: string) => {
 
 export const parseSec256K1 = (pem: string) => {
 
-  const raw = Buffer.from(pem, "base64")
-    .toString("hex")
-    .replace("30740201010420", "")
-    .replace("a00706052b8104000aa144034200", "")
+  const raw = Buffer.from(pem, 'base64')
+    .toString('hex')
+
+  if (!raw.substring(0, 24).includes(SEC256k1_KEY_INIT)) {
+    return undefined;
+  }
+
+  const trimRaw = raw
+    .replace(SEC256k1_KEY_INIT, '')
+    .replace(SEC256k1_KEY_SEPARATOR, '')
 
   try {
-    const key = new Uint8Array(Buffer.from(raw.substring(0, 64), "hex"));
+    const key = new Uint8Array(Buffer.from(trimRaw.substring(0, 64), 'hex'));
     const identity = Secp256k1KeyIdentity.fromSecretKey(key);
     const type = Types.pem256k1;
     return { identity, type };
@@ -41,10 +59,10 @@ export const parseSec256K1 = (pem: string) => {
 export const getIdentityFromPem = (pem) => {
 
   const trimedPem = pem
-    .replace(/(-{5}.*-{5})/g, "")
-    .replace("\n", "")
+    .replace(/(-{5}.*-{5})/g, '')
+    .replace('\n', '')
     // Sepk256k1 keys
-    .replace("BgUrgQQACg==", "")
+    .replace('BgUrgQQACg==', '')
     .trim();
 
   const parsedIdentity = parseEd25519(trimedPem) || parseSec256K1(trimedPem);
