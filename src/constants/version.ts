e2838d021383985ee4e369ffa4785ad7c198038d
Dante <dante@Dantes-MacBook-Pro.local>
2022-10-28 15:12:02 -0300

    Hotfix stringify identity

--- src/constants/version.ts
+++ src/constants/version.ts
@@ -1,1 +1,1 @@
-export const PLUG_CONTROLLER_VERSION = "0.24.5";
+export const PLUG_CONTROLLER_VERSION = "0.24.6";
--- src/utils/storage/update_handlers/v0.20.0.ts
+++ src/utils/storage/update_handlers/v0.20.0.ts
@@ -1,6 +1,13 @@
 import { createAccountFromMnemonic } from '../../../utils/account';
 import { Types } from '../../../utils/account/constants';
+import Secp256k1KeyIdentity from '../../identity/secpk256k1/identity';
 
+const getKeyPair = (identity: Secp256k1KeyIdentity) => {
+    const jsonIdentity = identity.toJSON();
+    return typeof jsonIdentity === 'string' 
+        ? jsonIdentity
+        : JSON.stringify(jsonIdentity);
+}
 
 export default (storage: any) => {
     const mnemonic = storage.mnemonic;
@@ -14,7 +21,7 @@ export default (storage: any) => {
                 return ({
                     ...wallet,
                     type: Types.mnemonic,
-                    keyPair: identity.toJSON(),
+                    keyPair: getKeyPair(identity),
                 })
             })
     }
